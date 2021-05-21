dfa80c24a011ce5ec665ccb69b1ad1d21d5890cb
dejon miller <danmil0@dejons-MacBook-Pro.local>
2021-05-21 00:03:13 -0400

    clean up

--- NoucheCoin.sol
+++ NoucheCoin.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.4;
 import "./Token.sol";
 import "./StandardToken.sol";
 
-//name this contract whatever you'd like
 contract NoucheToken is StandardToken {
 
     function () {
@@ -19,16 +18,10 @@ contract NoucheToken is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-    //
-    // CHANGE THESE VALUES FOR YOUR TOKEN
-    //
-
-    //make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = 'H1.0';
 
     function NoucheToken(
     ) {
@@ -44,9 +37,6 @@ contract NoucheToken is StandardToken {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
 
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
-        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
         if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
         return true;
     }
